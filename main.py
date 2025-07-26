22c9b26b2f99e808eaba0da9bfc16b0b19442f94
nmtrang29 <nguyenminhtrang29@gmail.com>
2025-07-26 19:37:47 +0200

    draw a player

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pygame
+from player import *
+from circleshape import *
 from constants import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     clock = pygame.time.Clock()
     dt = 0
 
@@ -11,11 +14,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip() 
 
         # limit the framerate to 60 FPS
         dt = clock.tick(60) * 000.1
-        
+    
 if __name__ == "__main__":
     main()
