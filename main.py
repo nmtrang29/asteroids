167391602c817e677bcf1b521aaa4f5fb91ac07e
nmtrang29 <nguyenminhtrang29@gmail.com>
2025-07-27 16:00:49 +0200

    add shooting and collision detection

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import pygame
+import pygame, sys
 from player import *
 from circleshape import *
 from constants import *
 from asteroid import *
 from asteroidfield import *
+from shot import *
 
 def main():
     pygame.init()
@@ -16,13 +17,16 @@ def main():
     asteroids = pygame.sprite.Group()
     asteroidfield = pygame.sprite.Group()
     
-  
+
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
     asteroidfield = AsteroidField()
 
-    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+    shots = pygame.sprite.Group()
+    Shot.containers = (shots, updatable, drawable)
 
 
     while True:
@@ -31,6 +35,15 @@ def main():
                 return
         
         updatable.update(dt)
+
+
+        for obj in asteroids:
+            if obj.collision_check(player):
+                print ("Game over!")
+                sys.exit()
+            for shot in shots:
+                if obj.collision_check(shot):
+                    obj.kill()
 
         screen.fill((0,0,0))
         
