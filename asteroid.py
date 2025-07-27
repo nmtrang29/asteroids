52d94de652bbb1ea9befe119171026e55e72a2be
nmtrang29 <nguyenminhtrang29@gmail.com>
2025-07-27 16:40:45 +0200

    add split mechanism for asteroids

--- asteroid.py
+++ asteroid.py
@@ -1,9 +1,11 @@
-import pygame
+import pygame, random
 from circleshape import *
+from constants import *
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
+        self.__has_splitted = False
 
 
     def draw(self, screen):
@@ -12,7 +14,18 @@ class Asteroid(CircleShape):
     def update(self, dt):
         self.position += self.velocity * dt 
         
-    
+    def split(self):
+        if self.radius == ASTEROID_MIN_RADIUS:
+            self.kill()
+        elif self.radius > ASTEROID_MIN_RADIUS:
+            self.kill()
+            random_angle = random.uniform(20, 50)
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            new_obj_1 = Asteroid(self.position.x, self.position.y, new_radius)
+            new_obj_2 = Asteroid(self.position.x, self.position.y, new_radius)
+            new_obj_1.velocity += self.velocity.rotate(random_angle) * 1.2
+            new_obj_2.velocity += self.velocity.rotate(-random_angle) * 1.2
 
+        
 
 
--- main.py
+++ main.py
@@ -36,14 +36,14 @@ def main():
         
         updatable.update(dt)
 
-
-        for obj in asteroids:
-            if obj.collision_check(player):
+        for asteroid in asteroids:
+            if asteroid.collision_check(player):
                 print ("Game over!")
                 sys.exit()
             for shot in shots:
-                if obj.collision_check(shot):
-                    obj.kill()
+                if asteroid.collision_check(shot):
+                    shot.kill()
+                    asteroid.split()
 
         screen.fill((0,0,0))
         
